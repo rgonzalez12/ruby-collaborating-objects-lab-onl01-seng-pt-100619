f32ec1dbe22015d8bb401a7df5137b3dc85fc493
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-07 02:17:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,5 +17,12 @@ class Song
     song
   end
   
-  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
 end
