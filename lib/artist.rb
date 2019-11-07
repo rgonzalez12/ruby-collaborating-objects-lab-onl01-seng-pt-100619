109ae07d3eea265c7d33d19cca77b6cf2741155c
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-07 02:54:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,6 +23,10 @@ class Artist
     self.find(name) ? self.find(name) : self.new(name)
   end
    
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
+  
   def print_songs
     songs.each {|song| puts song.name}
   end
