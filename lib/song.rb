ef8a3614595e36afd67d82a1666b9a1de4f44099
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-07 02:48:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,10 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = Artist.new
-    song.title = filename.split(" - ")[1]
-    song
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
   
   def artist_name=(name)
