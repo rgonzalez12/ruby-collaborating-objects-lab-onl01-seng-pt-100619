c3ab650c311fa62f0fabd815740c1c7d2be06654
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-07 02:46:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,12 @@ class Artist
     @@all
   end
   
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-  
   def add_song(song)
     song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
   
   def self.find_or_create_by_name(name)
